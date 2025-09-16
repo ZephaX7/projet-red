0274a8b7934ea71b1c91a87bab9500da5222032f
Claxid <hohojiji651@gmail.com>
2025-09-16 10:43:59 +0200

    Add files via upload

---
+++ projet-red_/src/Fight/Combat.go
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+package combat
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+
+func combat{
+	
+}
+=======
+package combat
+
+func combat() {
+
+}
+>>>>>>> 70960afda4ca951370b46da5059d752b6b762086
