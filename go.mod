0ff78c8ad0ef0492e67cdb28035fde3ff435b3e0
Antoine-Gury <antoinegury@outlook.fr>
2025-09-16 16:46:53 +0200

    recup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ZephaX7/projet-red
+
+go 1.25.0
--- src/Shop/Boutique.go
+++ src/Shop/Boutique.go
@@ -8,18 +8,17 @@ import (
 
 func Shop() {
 	items := []string{
-		"Potion de soin gratuite(C'est pour toi bg,cadeau de la maison. Par contre si tu l'uses pour rien, je vais te retrouver donc fais gaffe à toi)",
+		"Potion de soin gratuite (cadeau de la maison, mais une seule fois !)",
 		"Potion de soin",
 		"Potion de poison",
 		"Livre de Sort : Boule de feu",
 		"Fourrure de Loup",
 		"Peau de Troll",
 		"Cuir de Sanglier",
-		"Plume de Corebeaux",
+		"Plume de Corbeaux",
 	}
 	prices := []int{0, 3, 6, 25, 4, 7, 3, 1} // la première est gratuite
 	Gold := 100
-	bought := []string{}
 	freeTaken := false // pour l'article gratuit
 
 	for {
@@ -40,30 +39,34 @@ func Shop() {
 			continue
 		}
 
-		// Vérification de l'article gratuit
-		if prices[choice-1] == 0 {
+		itemName := items[choice-1]
+		itemPrice := prices[choice-1]
+
+		// Article gratuit
+		if itemPrice == 0 {
 			if freeTaken {
-				fmt.Println("Vous ne pouvez prendre cet article gratuit qu'une seule fois !")
-				continue
-			} else {
-				bought = append(bought, items[choice-1])
-				freeTaken = true
-				fmt.Println("Vous avez pris", items[choice-1])
+				fmt.Println("⚠ Vous ne pouvez prendre cet article gratuit qu'une seule fois !")
 				continue
 			}
+			if len(Inventory.Inventaire) >= Inventory.CapaciteMax {
+				fmt.Println("Votre inventaire est plein, impossible de prendre l'objet gratuit.")
+				continue
+			}
+			Inventory.AddInventory(Inventory.Objet{Nom: itemName, Quantite: 1, Type: "Objet"})
+			freeTaken = true
+			fmt.Println("Vous avez pris :", itemName)
+			continue
 		}
 
 		// Achat normal
-		if Gold >= prices[choice-1] {
-			// Vérifier si l'inventaire est plein
+		if Gold >= itemPrice {
 			if len(Inventory.Inventaire) >= Inventory.CapaciteMax {
 				fmt.Println("Votre inventaire est plein, vous ne pouvez rien acheter de plus.")
-				continue // retourne au début de la boucle pour choisir autre chose
+				continue
 			}
-
-			Gold -= prices[choice-1]
-			bought = append(bought, items[choice-1])
-			fmt.Println("Vous avez acheté", items[choice-1])
+			Gold -= itemPrice
+			Inventory.AddInventory(Inventory.Objet{Nom: itemName, Quantite: 1, Type: "Objet"})
+			fmt.Println("Vous avez acheté :", itemName)
 		} else {
 			fmt.Println("Vous n'avez pas assez d'or.")
 		}
